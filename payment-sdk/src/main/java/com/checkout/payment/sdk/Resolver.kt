19a5b4b4372304a32fbeb68b454cf445fd39ae2c
Siamak <ssiasoft@gmail.com>
2026-04-13 20:04:36 +0800

    Update Resolver.kt

--- payment-sdk/src/main/java/com/checkout/payment/sdk/Resolver.kt
+++ payment-sdk/src/main/java/com/checkout/payment/sdk/Resolver.kt
@@ -2,6 +2,12 @@ package com.checkout.payment.sdk
 
 import com.checkout.payment.api.Environment
 
+
+// These endpoints are provided as part of the public api ref so I'll leave them here to avoid some over engineering to hide publuc info.
+// In a real-world architecture, mobile clients should not directly
+// communicate with payment provider APIs. A backend should handle
+// all sensitive operations and environment configuration. or at least coveyed thru safe channels.
+
 internal fun Environment.baseUrl(): String = when (this) {
     Environment.SANDBOX -> "https://api.sandbox.checkout.com/"
     Environment.PRODUCTION -> "https://api.checkout.com/"
